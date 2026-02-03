37ee7db7cf3dd56ac0edac63dab3f2e62ca16a0c
akshpawar2003 <akashpawar111213@gmail.com>
2026-02-03 15:21:41 +0530

    update cloudiniary configuration because for vercel deployment diskstorage is not working

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      ""
+      "blog-application-ecru-iota.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
