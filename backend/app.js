0402330825da538d3e8ab159a5925c4ad6dbaa40
akshpawar2003 <akashpawar111213@gmail.com>
2026-02-03 10:52:45 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'blog-application-alpha-taupe.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
