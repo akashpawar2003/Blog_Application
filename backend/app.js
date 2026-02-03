4f10acbc44be89a3bc47c1ba79ec32b31c8f06b1
akshpawar2003 <akashpawar111213@gmail.com>
2026-02-03 10:46:24 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://blog-application-hazel-pi.vercel.app',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
